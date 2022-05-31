b8daf2af6dfcebcb002aec25ed02ef345c9c3097
matheustkaczyk <matheus111904@hotmail.com>
2022-05-31 15:57:42 -0300

    feat: fazendo ao clicar no botao enviar emitir para o socket evento de chat message enviando payload, ainda nao esta funcionando

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { io } from 'socket.io-client';
+import Button from '../components/Button';
 
 import Header from '../components/Header';
 import Input from '../components/Input';
@@ -7,8 +8,15 @@ import Input from '../components/Input';
 const Chat = () => {
   const socket = io('http://localhost:3001', { transports: ['websocket', 'polling', 'flashsocket'] });
 
+  const [message, setMessage] = useState('');
+
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    
+    setMessage(e.target.value);
+  }
+
+  const handleClick = () => {
+    socket.emit('chat message', message);
+    setMessage('');
   }
 
   return (
@@ -17,7 +25,8 @@ const Chat = () => {
     <main>
       <form>
         <ul></ul>
-        <Input type="text" placeholder='Digite uma mensagem' onChange={(e) => handleInput(e)}  />
+        <Input type="text" placeholder='Digite uma mensagem' onChange={(e) => handleInput(e)} value={message}  />
+        <Button text='Enviar' type="button" onClick={() => handleClick()} />
       </form>
     </main>
     </>
