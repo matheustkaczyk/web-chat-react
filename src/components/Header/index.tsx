a5e9ae67bd3f529532d01497d4aefa71c54a1d36
matheustkaczyk <matheus111904@hotmail.com>
2022-05-31 15:17:51 -0300

    feat: componentizando basico header, renderizando na tela

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return(
+    <header>
+      <h1>Web-chat</h1>
+    </header>
+  )
+}
+
+export default Header;
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,12 +1,16 @@
 import { io } from 'socket.io-client';
+import Header from '../components/Header';
 
 const Chat = () => {
   const socket = io('http://localhost:3001', { transports: ['websocket', 'polling', 'flashsocket'] });
 
   return (
-    <div>
-      <h1>Chat</h1>
-    </div>
+    <>
+    <Header />
+    <main>
+      
+    </main>
+    </>
   );
 }
 
