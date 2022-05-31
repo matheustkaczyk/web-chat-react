4dc1132cb5d8e180d15a910a64a578bb885049bb
matheustkaczyk <matheus111904@hotmail.com>
2022-05-31 16:12:12 -0300

    fix: corrigindo chamanda da Ref do socket, funcionando corretamente

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,21 +1,27 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 import Button from '../components/Button';
 
 import Header from '../components/Header';
 import Input from '../components/Input';
 
-const Chat = () => {
-  const socket = io('http://localhost:3001', { transports: ['websocket', 'polling', 'flashsocket'] });
 
+const Chat = () => {
   const [message, setMessage] = useState('');
+  const socketClientRef = useRef() as any;
+  
+  useEffect(() => {
+    var socket = io('http://localhost:3001', { transports: ['websocket', 'polling', 'flashsocket'] });
+
+    socketClientRef.current = socket;
+  }, []);
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
   }
 
   const handleClick = () => {
-    socket.emit('chat message', message);
+    socketClientRef.current.emit('message', message);
     setMessage('');
   }
 
