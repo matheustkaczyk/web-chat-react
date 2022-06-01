64d7e9400777e1acf633fd91b8420185d1c21b8b
matheustkaczyk <matheus111904@hotmail.com>
2022-06-01 10:03:49 -0300

    fix: refatorando, desacoplando socket, resolvendo problemas de rerenderizacoes multiplas

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,37 +1,29 @@
-import { ChangeEvent, useState, useEffect, useRef } from 'react';
-import { io } from 'socket.io-client';
+import { ChangeEvent, useState, useEffect } from 'react';
 import Button from '../components/Button';
 
 import Header from '../components/Header';
 import Input from '../components/Input';
 
+import socket from '../service/socket';
 
 const Chat = () => {
-  const [connected, setConnected] = useState(false);
   const [message, setMessage] = useState('');
-  const socketClientRef = useRef() as any;
-  
+
   useEffect(() => {
-    var socket = io('http://localhost:3001', { transports: ['websocket', 'polling', 'flashsocket'] });
+    socket.on('message', (msg: string) => {
+      const list = document.getElementById('list_element') as Element;
+      const li = document.createElement('li');
+      li.innerHTML = msg;
+      list.appendChild(li);
+    })
+  }, [socket])
 
-    socketClientRef.current = socket;
-    setConnected(true);
-  }, []);
-
-  connected && socketClientRef.current.on('message', (msg: string) => {
-    const list = document.getElementById('list_element') as Element;
-    console.log(list);
-    const li = document.createElement('li');
-    li.innerHTML = msg;
-    list.appendChild(li);
-  })
-  
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
   }
 
   const handleClick = () => {
-    socketClientRef.current.emit('userMessage', message);
+    socket.emit('userMessage', message);
     setMessage('');
   }
 
