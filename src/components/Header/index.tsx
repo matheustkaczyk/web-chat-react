45e2e7d7ecc879e94dd9fcafbc7fb533141f9a6d
matheustkaczyk <matheus111904@hotmail.com>
2022-06-01 17:04:04 -0300

    feat: adicionando imagem de gear, renderizando no header, colocando header display flex

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import chatSvg from '../../images/chat.svg';
+import gearSvg from '../../images/gear.svg';
 
 const Header = () => {
   return(
     <header>
       <img src={chatSvg} alt="Chat icon" />
+      <img src={gearSvg} alt="Gear icon" />
     </header>
   )
 }
