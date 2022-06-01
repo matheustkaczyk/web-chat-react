e35ccaeee50ed5132c75182a304ff9465ecf693e
matheustkaczyk <matheus111904@hotmail.com>
2022-06-01 09:43:15 -0300

    fix: removendo strictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Chat from './pages/Chat'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <Chat />
-  </React.StrictMode>
 )
