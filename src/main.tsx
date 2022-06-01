22554d010bbce6de0ebe70422327f6ff73116c5e
matheustkaczyk <matheus111904@hotmail.com>
2022-06-01 10:12:09 -0300

    feat: instalando sass, arrumando importacao

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import Chat from './pages/Chat'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
