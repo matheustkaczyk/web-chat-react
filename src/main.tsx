0469ed60560dcd94e9be664e1abc3c4cd7ca5f7d
matheustkaczyk <matheus111904@hotmail.com>
2022-06-02 10:11:13 -0300

    feat: instalando react-router-dom, criando rota de 'login'

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.scss'
 import Chat from './pages/Chat'
+import Login from './pages/Login'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Chat />
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/chat' element={<Chat />} />
+        </Routes>
+    </BrowserRouter>
 )
