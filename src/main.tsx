82b5628a1724b3aa61718661d7f7eecc331fa7b5
matheustkaczyk <matheus111904@hotmail.com>
2022-06-02 10:13:48 -0300

    fix: revertendo, tirando react router, mudei de ideia

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.scss'
 import Chat from './pages/Chat'
-import Login from './pages/Login'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/chat' element={<Chat />} />
-        </Routes>
-    </BrowserRouter>
+    <Chat />
 )
