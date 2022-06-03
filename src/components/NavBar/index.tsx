6af24490de8da5b719d37370424ff1583d5b2286
matheustkaczyk <matheus111904@hotmail.com>
2022-06-03 12:02:30 -0300

    feat: adicionando imagem edit, preparando terreno para implementacao da mudança de nick

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,11 +1,18 @@
+import { Suspense, useState } from "react";
+import Input from "../Input";
 import Select from "../Select";
 import INavBar from "./navbar.interface";
+import editIcon from '../../images/edit.svg';
 
 const NavBar = ({ username, handleChange, handleSideBar }: INavBar) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const Options = ['Geral', 'Games', 'Programação', 'Filmes', 'Séries'];
+
   return(
     <nav onMouseLeave={() => handleSideBar()}>
-      <h3>{username}</h3>
+      <h3>{username} {<img src={editIcon} alt="Edit icon"/>}</h3>
+      {isOpen === true && <Input type="text" onChange={() => {}} placeholder="Mude seu username"  />}
       <div>
         <Select label="Escolha uma sala" handleChange={handleChange} options={Options} id="category" />
       </div>
