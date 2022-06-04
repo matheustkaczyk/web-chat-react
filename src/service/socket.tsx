34b6d858b5f9ebfa40825f58a9c22ea405af3ed2
matheustkaczyk <matheus111904@hotmail.com>
2022-06-04 10:05:50 -0300

    fix: removendo configuracoes nao utilizadas do socket

--- src/service/socket.tsx
+++ src/service/socket.tsx
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
 
-export default io('https://webchatbackendtkaczyk.herokuapp.com/', { transports: ['websocket', 'polling', 'flashsocket'] });
+export default io('https://webchatbackendtkaczyk.herokuapp.com/', { transports: ['websocket'] });
