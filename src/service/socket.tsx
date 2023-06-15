70e16c061552d62a4f7eb79b8ce0708cac16b6b3
Matheus Tkaczyk Ribeiro <82241753+matheustkaczyk@users.noreply.github.com>
2023-06-15 09:13:59 -0300

    fix: changing backend endpoint

--- src/service/socket.tsx
+++ src/service/socket.tsx
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
 
-export default io('https://webchatbackendtkaczyk.herokuapp.com/', { transports: ['websocket'] });
+export default io('https://web-chat-backend-production.up.railway.app', { transports: ['websocket'] });
